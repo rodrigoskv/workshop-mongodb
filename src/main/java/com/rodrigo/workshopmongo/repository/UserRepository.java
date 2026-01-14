78abd1362ee728da6a3ccbdae4e164aa1cd952e6
rodrigoskv <rodrigotelesskv@gmail.com>
2026-01-14 16:50:26 -0300

    add User management functionality with User, UserRepository, UserService, and UserResource classes

---
+++ src/main/java/com/rodrigo/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.rodrigo.workshopmongo.repository;
+
+import com.rodrigo.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
