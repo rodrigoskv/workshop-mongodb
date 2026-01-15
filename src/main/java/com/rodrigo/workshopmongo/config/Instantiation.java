2f50f50ae7cef2ca32d80d16156e4ba22e0bc485
rodrigoskv <rodrigotelesskv@gmail.com>
2026-01-15 16:42:38 -0300

    add posts relationship to User and update Instantiation with sample posts

--- src/main/java/com/rodrigo/workshopmongo/config/Instantiation.java
+++ src/main/java/com/rodrigo/workshopmongo/config/Instantiation.java
@@ -39,10 +39,14 @@ public class Instantiation implements CommandLineRunner {
         userRepository.save(alex);
         userRepository.save(bob);
 
-        Post post = new Post(null, sdf.parse("21/03/2026"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post1 = new Post(null, sdf.parse("21/03/2026"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2026"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
-        postRepository.save(post);
+        postRepository.save(post1);
         postRepository.save(post2);
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
+
     }
 }
--- src/main/java/com/rodrigo/workshopmongo/domain/User.java
+++ src/main/java/com/rodrigo/workshopmongo/domain/User.java
@@ -1,11 +1,14 @@
 package com.rodrigo.workshopmongo.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.FieldType;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document(collection = "user")
@@ -18,6 +21,9 @@ public class User implements Serializable {
 
     private String name;
     private String email;
+
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
 
     public User(){
     }
@@ -50,6 +56,14 @@ public class User implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
     @Override
